8f022f7f96d1d8cf7821b1bbe2828647d19dd0e9
Luciano <lupel31@gmail.com>
2020-11-19 00:18:29 -0300

    added pause between actions and the option to inform more than one channel

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-require('dotenv').config({path: './auth.env'});
-const _tmi = require('tmi.js');
-const _fs = require('fs');
+require( 'dotenv' ).config({ path: './auth.env' });
+const _tmi = require( 'tmi.js' );
+const _fs  = require( 'fs' );
+
+const sleep = async ( waitTimeInMs ) => new Promise( resolve => setTimeout( resolve, waitTimeInMs ) );
 
 const globals = {
   script: process.argv[1],
@@ -8,48 +10,51 @@ const globals = {
   file: process.argv[3]
 }
 
-// Create option object. Contains configuration for _tmi.client()
 const options = {
   options: { debug: false },
   connection: { reconnect: true, secure: true },
   identity: { username: process.env.TWITCH_USERNAME, password: process.env.TWITCH_OAUTH },
-  channels: [ globals.channel ]
+  channels: globals.channel.split( '/' )
 }
 
-// Construct new client with options [Object];
-// Export client out of module as 'client'
 const client = new _tmi.client(options);
-
-// Connect previously constructed client
 client.connect();
+client.on( 'connected', doIt );
 
-client.on('connected', () => {
-  doIt();
-});
 
-function doIt() {
-  _fs.readFile(globals.file, 'utf8', (error, fileData) => {
-    if (error) throw error;
+const timeToWait = 1;
 
-    let banList = fileData.replace(/\r\n/g, ' ').split(' ');
+
+async function doIt(){
+  _fs.readFile( globals.file, 'utf8', async ( error, fileData )=>{
+    if( error ){
+      throw error;
+    }
+
+    let banList  = fileData.replace( /\r\n/g, ' ' ).split( ' ' );
     let banQueue = banList;
 
-    if (banQueue.length != 0) {
-      let channel = globals.channel.toString();
-      do {
-        /*_twitch.client.ban(channel, banQueue[0].toString(), `PM ${process.env.TWITCH_USERNAME} about why this user was banned. Automated with Twitch Mass Ban by OscarXcore.`)
-        .then((data) => {
-          console.log(`${data[0]}: ${data[1]} is now banned. Reason: ${data[2]}`);
-        }).catch((error) => {
-          console.log(error);
-        })*/
-        client.say(channel, `/ban ${banQueue[0].toString()} "PM ${process.env.TWITCH_USERNAME} about why this user was banned. Automated with Twitch Mass Ban by Stapleton on Github."`)
-        console.log(`#${channel}: User #${banQueue.length} | ${banQueue[0].toString()} has been banned from the channel.`);
+    if( banQueue.length!=0 ){
+      let _channels = globals.channel.split( '/' );
+
+      do{
+        for( let _c=0; _c<_channels.length; _c=_c+1 ){
+          _channel = _channels[_c].toString();
+
+          client.say( _channel, `/ban ${banQueue[0].toString()} "${banQueue[0].toString()} Auto Ban"` )
+          console.log( `#${_channel} | ${banQueue[0].toString()} Auto Ban` );
+          await sleep( timeToWait*1000 );
+        };
+
+        console.log( ' ' );
+
         banQueue.shift();
-      } while (banQueue.length != 0);
+      }while( banQueue.length!=0 );
     }
-	if (banQueue.length == 0) {
-		console.log('\nFinished. Press CTRL+C to exit.');
+
+	if( banQueue.length==0 ){
+      console.log( '\nFinished.' );
+      process.exit( 0 );
 	}
   });
 }
